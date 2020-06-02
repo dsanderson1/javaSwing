9288d2f5b37e02975b412c537a8e4fb5ab6c9425
dsanderson1 <33199566+dsanderson1@users.noreply.github.com>
2020-06-02 11:14:27 +0100

    first commit test

---
+++ src/com/javaswing/databaseConnection.java
@@ -0,0 +1,20 @@
+package com.javaswing;
+import java.sql.*;
+
+
+public class databaseConnection {
+    String url = "jdbc:mysql://localhost:3306/additiontest?autoReconnect=true&useSSL=false";
+    String username = "danny";
+    String password = "danny";
+    String query;
+
+    public databaseConnection(String query){
+        this.query = query;
+    }
+
+    public void runQuery() throws SQLException {
+        Connection conn = DriverManager.getConnection(url,username, password);
+        Statement st = conn.createStatement();
+        st.executeUpdate(query);
+    }
+}
